9ae0ecd8eceabf8cadb923619953c46d860f1c16
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-05-17 14:47:56 -0400

    updated urls, views, css, etc

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -5,7 +5,7 @@ def starting_page(request):
    return render (request, "blog/index.html") # we added "templates" to settings.py, so Django will look there, we just need to specify blog/index.html
 
 def posts(request):
-    pass
+    return render(request, "blog/all-posts.html")
 
-def post_detail(requset):
-    pass
+def post_detail(request, slug):
+    return render(request, "blog/post-detail.html")
