d354b83c904cea81313758751d00b59e616b7d38
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-05-20 16:54:13 -0400

    finished with admin

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -5,7 +5,7 @@ from .models import Post, Author, Tag
 class PostAdmin (admin.ModelAdmin):
     list_filter = ("author", "tags", "date", )
     list_display = ("title", "date", "author",)
-    prepopulated_field = ("title",)
+    prepopulated_fields = {"slug":("title",)}
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
