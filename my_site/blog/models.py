7112cf3ccf57241cda9bfee249771b42705a3193
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-05-20 16:23:45 -0400

    admin output

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -14,6 +14,14 @@ class Author(models.Model):
     last_name=models.CharField(max_length=50)
     email=models.EmailField() #max_length is not required (it's optional)
     
+    def full_name (self):
+        return f"{self.first_name}{self.last_name}"
+    
+    def __str__(self):
+        return self.full_name()
+    
+    
+    
     
 class Post(models.Model):
     title = models.CharField(max_length=100)
